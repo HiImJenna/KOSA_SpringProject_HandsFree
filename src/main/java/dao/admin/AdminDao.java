4767412253861bff54f25d4087a069069bf04f6d
Youngnam Lee <lyn00227@naver.com>
2023-02-14 18:30:36 +0900

    점주 상세정보 등록

--- src/main/java/dao/admin/AdminDao.java
+++ src/main/java/dao/admin/AdminDao.java
@@ -1,15 +1,13 @@
 package dao.admin;
 
-import java.sql.SQLException;
-
-import org.springframework.transaction.annotation.Transactional;
-
 import vo.admin.Admin;
 import vo.admin.Store;
+import vo.admin.StoreDetails;
 import vo.admin.StoreKeeper;
 
 public interface AdminDao {
 	public int registerAdmin(Admin admin);
 	public int registerStore(Store store);
 	public int registerStoreKeeper(StoreKeeper keeper);
+	public int registerStoreDetails(StoreDetails storeDetails);
 }
