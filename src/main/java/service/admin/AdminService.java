2c0b5aca6627652484432fd150f1dc085f4f250b
Greatbok <simso108@gmail.com>
2023-02-15 19:06:29 +0900

    마이페이지 저장 xml 작업중

--- src/main/java/service/admin/AdminService.java
+++ src/main/java/service/admin/AdminService.java
@@ -26,4 +26,20 @@ public class AdminService {
 		dao.registerStore(store);
 		return dao.registerStoreDetails(storeDetails);
 	}
+	
+	public Admin getAdmin(String userid) {
+		AdminDao dao = sqlsession.getMapper(AdminDao.class);
+		Admin admin = dao.getAdmin(userid);
+		return admin;
+	}
+
+	public void updateAdmin(Admin admin) {
+		AdminDao dao = sqlsession.getMapper(AdminDao.class);
+		int result = dao.updateAdmin(admin);
+		if(result > 0) {
+			System.out.println("업데이트 성공");
+		} else {
+			System.out.println("업데이트 실패");
+		}
+	}
 }
