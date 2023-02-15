66e62288ceaa3ddefbb2a4b971a858b7a89d02cc
nhj <nhj6336@gmail.com>
2023-02-15 11:03:03 +0900

    지도로케이션 백단처리

--- src/main/java/controller/user/UsermainController.java
+++ src/main/java/controller/user/UsermainController.java
@@ -1,13 +1,25 @@
 package controller.user;
 
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import service.user.UserSearchService;
 
 @Controller
 public class UsermainController {
 	
+	@Autowired
+	private UserSearchService userSearchService;
 	
 	@GetMapping("search")
 	public String home(HttpServletRequest httpServletRequest, Model model) {
@@ -22,5 +34,19 @@ public class UsermainController {
 		return "/user/search";
 	}
 	
-	
+	@GetMapping("selectStore")
+	public ResponseEntity<?> getStore(@RequestParam Map<String, String> position){
+		System.out.println(position);
+		System.out.println(position.get("latitude"));
+		System.out.println(position.get("longitude"));
+		String latitude = position.get("latitude");
+		String longitude = position.get("longitude");
+		List<Map<String, String>> list =  userSearchService.getStore("127.458855318689", "36.6371176461081");
+		System.out.println(list);
+		System.out.println(list.toString());
+
+		
+		return new ResponseEntity<>(list, HttpStatus.OK);
+		
+	}
 }
