e7ff2c49b103f929b28ff6ac4914e2691081bbf2
HiImJenna <skygarden316@naver.com>
2023-02-21 14:47:04 +0900

    사용자 url로 storeid 가져오기 성공

--- src/main/java/dao/user/PaymentDao.java
+++ src/main/java/dao/user/PaymentDao.java
@@ -1,11 +1,8 @@
 package dao.user;
 
 import java.sql.SQLException;
-
 import org.apache.ibatis.annotations.Param;
-
 import vo.admin.Store;
-import vo.admin.StoreDetails;
 import vo.user.Payment;
 
 public interface PaymentDao {
@@ -13,5 +10,5 @@ public interface PaymentDao {
 	//결제
 	public int insertPayment(Payment payment) throws ClassNotFoundException, SQLException;
 		
-	public Store findStoreByStoreId(@Param("storeid") String storeId);
+	public Store findStoreByStoreId(@Param("STOREID") String storeId);
 }
