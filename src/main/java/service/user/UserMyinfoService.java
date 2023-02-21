5778843191b8014ad93a20eb880a6bd1527b7b64
HiImJenna <skygarden316@naver.com>
2023-02-21 23:27:35 +0900

    사용자 예약 내역 불러오기 구현

--- src/main/java/service/user/UserMyinfoService.java
+++ src/main/java/service/user/UserMyinfoService.java
@@ -1,28 +1,19 @@
 package service.user;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.sql.SQLException;
-
-import javax.servlet.ServletOutputStream;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import dao.user.UserMyinfoDao;
+import vo.user.Payment;
 import vo.user.Users;
 
 @Service
 public class UserMyinfoService {
 	
-	// 임시 사라질 수 있음
-	private final String PATH = "/files/upload";
-
 	@Autowired
 	private SqlSession sqlsession;
 
@@ -66,52 +57,24 @@ public class UserMyinfoService {
 		return result;
 		
 	}
-
-	// 내 정보 수정하기 처리 서비스
-//	public void noticeUpdate(Users users) {
-//		if (!users.getProfile_path().equals("")) {
-//			String fpath = users.getRealFilePath();
-//			FileOutputStream fs = null;
-//			try {
-//				
-//				File dir = new File(users.getRealFilePath());
-//				if (!dir.isDirectory()) {
-//					dir.mkdir();
-//				}
-//				
-//				fs = new FileOutputStream(fpath + users.getProfile_path().getOriginalFilename());
-//				fs.write(users.getProfile_path().getBytes());
-//			} catch (Exception e) {
-//				e.printStackTrace();
-//			} finally {
-//				try {
-//					fs.close();
-//				} catch (Exception e2) {
-//					e2.printStackTrace();
-//				}
-//			}
-//		}
-//	}
 	
-	// 파일 다운로드 서비스 함수 -> 다운로드까지 할 지 말 지 물어보기
-	public void download(String p, String f, HttpServletRequest request, HttpServletResponse response)
-			throws IOException {
-
-		String fname = new String(f.getBytes("euc-kr"), "8859_1");
-		response.setHeader("Content-Disposition", "attachment;filename=" + fname + ";");
-
-		String fullpath = request.getServletContext().getRealPath("/customer/" + p + "/" + f);
-		FileInputStream fin = new FileInputStream(fullpath);
-
-		ServletOutputStream sout = response.getOutputStream();
-		byte[] buf = new byte[1024]; // 전체를 다읽지 않고 1204byte씩 읽어서
-		int size = 0;
-		while ((size = fin.read(buf, 0, buf.length)) != -1) {
-			sout.write(buf, 0, size);
+	//예약 내역 상세보기
+	public List<Map<String, String>> reserveInfo(String userid) {
+		List<Map<String, String>> list = null;
+		Map map = null;
+		
+		try {
+		 	UserMyinfoDao userdao = sqlsession.getMapper(UserMyinfoDao.class);
+		 	list = userdao.reserveInfo(userid);
+		} catch (ClassNotFoundException e) {
+						e.printStackTrace();
+		} catch (SQLException e) {
+						e.printStackTrace();
 		}
-		fin.close();
-		sout.close();
-	}
+		System.out.println("myinfo서비스 list : " + list);
 
+		return list;
+		
+	}
 
 }
